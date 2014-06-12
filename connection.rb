7305a835d599db9be2732bd962686fb5779ad066
Piotr Olchawa <piotrekolchawa@gmail.com>
2014-06-12 21:56:06 +0200

    makes logs more readable

--- connection.rb
+++ connection.rb
@@ -1,39 +1,41 @@
-SERVER_PORT = 2014
+DEFAULT_RPORT = 2014
 
 class Connection
   attr_accessor :my_ip
 
   require 'socket'
 
-  def initialize ip, s_port
-    @master_ip = ip
+  def initialize master_ip = nil, master_rPort = nil, my_rPort = DEFAULT_RPORT
+    @master_ip = master_ip
+    @master_rPort = master_rPort
+    @my_ip = local_ip
+    @my_rPort = my_rPort
     @age = Time.now.to_i
-
     # if there is no master or you can't connect to it - become one
     become_a_master if @master_ip.nil? or !get_server_list!
-
-    @server = TCPServer.new(s_port.nil? ? SERVER_PORT : s_port)
+    # start service server
+    @server = TCPServer.new(my_rPort.nil? ? DEFAULT_RPORT : my_rPort)
+    puts "log: Started service server at #{@my_ip}:#{@my_rPort}"
   end
 
   def become_a_master
     @master = true
-    @my_ip = local_ip
     @master_ip = @my_ip
     @server_list = [@my_ip]
-    p "become master"
+    puts "log: Became master server!"
   end
   def master?; return @master; end
 
   def get_server_list!
     begin
-      initSession = TCPSocket.new(@master_ip, SERVER_PORT)
+      initSession = TCPSocket.new(@master_ip, @master_rPort)
       initSession.puts "hello\n"
       @server_list = initSession.gets.split(" ")
       initSession.close
-      puts "Aquaired server list: #{@server_list.to_s}"
+      puts "log: Aquaired server list: #{@server_list.to_s}"
       return true
     rescue
-      puts "Couldn't connect to the master server"
+      puts "log: Couldn't connect to the master server!"
       return false
     end
   end
@@ -45,7 +47,7 @@ class Connection
         input = session.gets
         puts input
 
-        # here we define t
+        # here we define the behaviour of the server
         case input
         when "hello\n" then
           session.puts @server_list.join(" ")
--- test2.rb
+++ test2.rb
@@ -1,4 +1,4 @@
 require_relative 'connection'
 
-#d = Connection.new("192.168.0.102", 2014)
-d = Connection.new("127.0.0.1", 2014)
+d = Connection.new("192.168.0.102", 2014)
+#d = Connection.new("127.0.0.1", 2014)
